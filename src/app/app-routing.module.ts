c59858f7d8551ac60fc1e2640b7dcd06e0e07957
mmadel <mohammed.adel82@gmail.com>
2023-05-21 12:09:10 +0300

    create review module route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultLayoutComponent } from './core/reviewlayout/default-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+    data: {
+      title: 'Review'
+    },
+    children: [
+      {
+        path: 'review',
+        loadChildren: () =>
+          import('./modules/review/pateint-review.module').then((m) => m.PateintReviewModule)
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,74 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IconModule } from '@coreui/icons-angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  DefaultLayoutComponent,
+  DefaultHeaderComponent,
+  DefaultFooterComponent,
+} from './core/reviewlayout';
+import {
+  AvatarModule,
+  BadgeModule,
+  BreadcrumbModule,
+  ButtonGroupModule,
+  ButtonModule,
+  CardModule,
+  DateRangePickerModule,
+  DropdownModule,
+  FooterModule,
+  FormModule,
+  GridModule,
+  HeaderModule,
+  ListGroupModule,
+  NavModule,
+  ProgressModule,
+  SharedModule,
+  SidebarModule,
+  TabsModule,
+  UtilitiesModule,
 
+
+
+} from '@coreui/angular-pro';
+const APP_CONTAINERS = [
+  DefaultHeaderComponent,
+  DefaultFooterComponent,
+  DefaultLayoutComponent
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ...APP_CONTAINERS
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,AvatarModule,
+    BadgeModule,
+    BreadcrumbModule,
+    ButtonGroupModule,
+    ButtonModule,
+    CardModule,
+    DropdownModule,
+    FooterModule,
+    FormModule,
+    GridModule,
+    HeaderModule,
+    ListGroupModule,
+    NavModule,
+    ProgressModule,
+    SharedModule,
+    SidebarModule,
+    TabsModule,
+    UtilitiesModule,
+    ReactiveFormsModule,
+    IconModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    DateRangePickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
