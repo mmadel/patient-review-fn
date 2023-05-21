d8908bb261f356693403e357a12538dfec538347
mmadel <mohammed.adel82@gmail.com>
2023-05-21 11:34:29 +0300

    organize import and declare components in module

--- src/app/modules/review/pateint-review.module.ts
+++ src/app/modules/review/pateint-review.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PateintReviewRoutingModule } from './pateint-review-routing.module';
-import { PatientReviewComponent } from './components/patient.review/patient-review.component';
-import { PatientSubmitionComponent } from './components/patient.submition/patient-submition.component';
 
 
+import {
+  PatientReviewComponent,
+  PatientSubmitionComponent
+} from './index'
+
+const APP_REVIEw_COMPONENTS = [
+  PatientReviewComponent,
+  PatientSubmitionComponent
+]
 @NgModule({
   declarations: [
-    PatientReviewComponent,
-    PatientSubmitionComponent
+    APP_REVIEw_COMPONENTS
   ],
   imports: [
     CommonModule,
