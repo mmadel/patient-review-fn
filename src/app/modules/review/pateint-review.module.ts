b3a03481c40c7348f9e4a4f32d4478491b552f3f
mmadel <mohammed.adel82@gmail.com>
2023-05-21 11:22:49 +0300

    create patient review module

---
+++ src/app/modules/review/pateint-review.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PateintReviewRoutingModule } from './pateint-review-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PateintReviewRoutingModule
+  ]
+})
+export class PateintReviewModule { }
