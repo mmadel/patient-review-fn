44cceaf3d1de801bb606f3e55a85dca5cab60074
mmadel <mohammed.adel82@gmail.com>
2023-05-22 12:17:23 +0300

    impl improve component

--- src/app/modules/review/components/improve.review/improve-review.component.ts
+++ src/app/modules/review/components/improve.review/improve-review.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./improve-review.component.css']
 })
 export class ImproveReviewComponent implements OnInit {
-
+  toggle = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  enableDisableRule(job: any) {
+    this.toggle = !this.toggle;
   }
 
 }
