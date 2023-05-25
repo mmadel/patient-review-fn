7ffb3425956923cc1972aeb062a3df81a85f1933
mmadel <mohammed.adel82@gmail.com>
2023-05-25 11:49:53 +0300

    import needed dependencies to feedback module

--- src/app/modules/feedback/feedback.module.ts
+++ src/app/modules/feedback/feedback.module.ts
@@ -1,14 +1,58 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { IconModule } from '@coreui/icons-angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { NgxSpinnerModule } from 'ngx-spinner';
 import { FeedbackRoutingModule } from './feedback-routing.module';
+import {
+  faStar as fasStar
+  , faPhoneVolume as faPhoneVolume
+  , faCreditCard as faCreditCard
+  , faTimeline as faTimeline
+  , faChildReaching as faChildReaching
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  faGithubAlt as faGithubAlt,
+  faProductHunt as faProductHunt,
+  faAdn as faAdn
+} from '@fortawesome/free-brands-svg-icons';
+import {
+  AlertModule, ButtonModule,
+  CardModule, FormModule, GridModule,
+  SharedModule,
+  UtilitiesModule,
+  ButtonGroupModule,
+  ProgressModule
+} from '@coreui/angular-pro';
+const APP_COMPONENTS = [
 
-
+]
 @NgModule({
-  declarations: [],
+  declarations: [
+    
+  ],
   imports: [
     CommonModule,
-    FeedbackRoutingModule
+    FeedbackRoutingModule,
+    ButtonModule,
+    CardModule,
+    FormModule,
+    GridModule,
+    SharedModule,
+    UtilitiesModule,
+    ButtonGroupModule,
+    IconModule,
+    ReactiveFormsModule,
+    ProgressModule,
+    FontAwesomeModule,
+    FormsModule,
+    NgxSpinnerModule.forRoot({ type: 'ball-scale-multiple' })
   ]
 })
-export class FeedbackModule { }
+export class FeedbackModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(fasStar, faGithubAlt, faPhoneVolume, faCreditCard, faProductHunt, faTimeline, faChildReaching, faAdn);
+  }
+}
