1c5fdeb572ef583bc1d3c914f053e0e547cf708e
mmadel <mohammed.adel82@gmail.com>
2023-05-25 16:18:25 +0300

    select feedback for each question

--- src/app/modules/feedback/components/patient.feedback/patient-feedback.component.ts
+++ src/app/modules/feedback/components/patient.feedback/patient-feedback.component.ts
@@ -6,16 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient-feedback.component.css']
 })
 export class PatientFeedbackComponent implements OnInit {
+  hospitalityToggle: any = {
+    vgood: false,
+    good: false,
+    vsad: false,
+    sad: false
+  }
+  clinicalToggle: any = {
+    vgood: false,
+    good: false,
+    vsad: false,
+    sad: false
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
-  next(event: any) {
+  submit() {
 
   }
-  submit(){
-    
+
+  public selectFeedback(event: any) {
+    var controlName: string[] = event.target.name.split("-", 2);    
+    var feedbackFeeling = controlName[1];
+    var entityContainer: any = controlName[0] === 'hospitality' ? this.hospitalityToggle : this.clinicalToggle;
+    console.log(feedbackFeeling)
+    switch (feedbackFeeling) {
+      case "VeryGood":
+        entityContainer.vgood = true;
+        entityContainer.good = false, entityContainer.vsad = false, entityContainer.sad = false
+        break;
+      case "Happy":
+        entityContainer.good = true;
+        entityContainer.vgood = false, entityContainer.vsad = false, entityContainer.sad = false
+        break;
+      case "Meh":
+        entityContainer.sad = true;
+        entityContainer.vgood = false, entityContainer.good = false, entityContainer.vsad = false
+        break;
+      case "Frown":
+        entityContainer.vsad = true;
+        entityContainer.vgood = false, entityContainer.good = false, entityContainer.sad = false
+        break;
+    }
   }
 
 }
