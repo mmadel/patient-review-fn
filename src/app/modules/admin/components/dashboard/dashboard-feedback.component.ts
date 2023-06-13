3dc7048ec6f19372f3930df0171e80cd6e24eaa4
mmadel <mohammed.adel82@gmail.com>
2023-06-13 19:03:29 +0300

    clean up dashboard component

--- src/app/modules/admin/components/dashboard/dashboard-feedback.component.ts
+++ src/app/modules/admin/components/dashboard/dashboard-feedback.component.ts
@@ -10,40 +10,13 @@ import { DashboardChartsData, IChartProps } from './dashboard-charts-data';
   styleUrls: ['./dashboard-feedback.component.scss']
 })
 export class DashboardFeedbackComponent implements OnInit {
-  public trafficRadioGroup = new UntypedFormGroup({
-    trafficRadio: new UntypedFormControl('Month')
-  });
-  public mainChart: IChartProps = {};
-  feedbackNumbers: FeedbackNumbers = {
-    total: 0,
-    goodFeedbackTotal: 0,
-    sadFeedbackTotal: 0,
-    vgoodFeedbackTotal: 0,
-    vsadFeedbackTotal: 0,
-    vgoodFeedbackPercentage: 0,
-    goodFeedbackPercentage: 0,
-    vsadFeedbackPercentage: 0,
-    sadFeedbackPercentage: 0
-  }
-  constructor(private chartsData: DashboardChartsData
-    , private dashboardFeedbackService: DashboardFeedbackService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    var startDate = moment(new Date()).startOf('day').valueOf();
-    var endDate = moment(new Date()).endOf('day').valueOf();
-    this.dashboardFeedbackService.getFeedbackNumbers(startDate, endDate).subscribe((response) => {
-      if (response.body !== null)
-        this.feedbackNumbers = response.body;
-        console.log(JSON.stringify(this.feedbackNumbers))
-    })
-    this.initCharts();
+
   }
   initCharts(): void {
-    this.mainChart = this.chartsData.mainChart;
+
   }
-  setTrafficPeriod(value: string): void {
-    this.trafficRadioGroup.setValue({ trafficRadio: value });
-    this.chartsData.initMainChart(value);
-    this.initCharts();
-  }
+
 }
