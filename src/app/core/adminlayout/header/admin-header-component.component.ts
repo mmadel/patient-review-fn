9dd5b5015836cb8c594fe49244a83eb32aab7c92
mmadel <mohammed.adel82@gmail.com>
2023-06-13 20:09:41 +0300

    add date range selector component in header and impl its change event functions

--- src/app/core/adminlayout/header/admin-header-component.component.ts
+++ src/app/core/adminlayout/header/admin-header-component.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
-
+import * as moment from 'moment';
 import { ClassToggleService, HeaderComponent } from '@coreui/angular-pro';
 import { ClinicService } from 'src/app/modules/admin/services/clinic/clinic.service';
 @Component({
@@ -18,7 +18,35 @@ export class AdminHeaderComponentComponent extends HeaderComponent {
   public themeSwitch = new UntypedFormGroup({
     themeSwitchRadio: new UntypedFormControl('light'),
   });
-
+  startDate: number | undefined;
+  endDate: number | undefined;
+  public customRanges = {
+    Today: [new Date(), new Date()],
+    Yesterday: [
+      new Date(new Date().setDate(new Date().getDate() - 1)),
+      new Date(new Date().setDate(new Date().getDate() - 1))
+    ],
+    'Last 7 Days': [
+      new Date(new Date().setDate(new Date().getDate() - 6)),
+      new Date(new Date())
+    ],
+    'Last 30 Days': [
+      new Date(new Date().setDate(new Date().getDate() - 29)),
+      new Date(new Date())
+    ],
+    'This Month': [
+      new Date(new Date().setDate(1)),
+      new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0)
+    ],
+    'Last Month': [
+      new Date(new Date().getFullYear(), new Date().getMonth() - 1, 1),
+      new Date(new Date().getFullYear(), new Date().getMonth(), 0)
+    ],
+    'Clear': [
+      0,
+      0
+    ]
+  };
   constructor(private classToggler: ClassToggleService ,private clinicService:ClinicService) {
     super();
   }
@@ -30,5 +58,12 @@ export class AdminHeaderComponentComponent extends HeaderComponent {
   setSelectedClinic(event: any) {
     this.clinicService.selectedClinic$.next(event.target.value)
   }
-
+  startDateChange(event: any) {
+    console.log('startDateChange');
+    this.startDate = event ? moment(new Date(event)).startOf('day').valueOf() : 0;
+  }
+  endDateChange(event: any) {
+    console.log('endDateChange');
+    this.endDate = event ? moment(new Date(event)).endOf('day').valueOf() : 0;
+  }
 }
