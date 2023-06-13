74cb9b2ace7edac8c5dbc6d04837139fc1983525
mmadel <mohammed.adel82@gmail.com>
2023-06-13 21:27:15 +0300

    create performance index service and impl get function

---
+++ src/app/modules/admin/services/performance/performance-index.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { PerformanceIndexContainer } from '../../models/performance.index/performance.index.container';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PerformanceIndexService {
+  private userUrl = environment.baseURL + 'performance/index/'
+  constructor(private http: HttpClient) { }
+
+  get(clinicId: number, startDate: number, endDate: number) {
+    return this.http.get<PerformanceIndexContainer>(`${this.userUrl}` + '/get/startDate/' + startDate + '/endDate/' + endDate + '/clinicId/' + clinicId, { observe: 'response' })
+  }
+
+}
