9db84981d5a3e35baa953edaab08b33bf529b10f
mmadel <mohammed.adel82@gmail.com>
2023-06-19 18:07:33 +0300

    load hospitality data from dashboard component

--- src/app/modules/admin/components/dashboard/charts/charts.components/hospitality/chart-hospitality.component.ts
+++ src/app/modules/admin/components/dashboard/charts/charts.components/hospitality/chart-hospitality.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
-import { PerformanceIndexService } from 'src/app/modules/admin/services/performance/performance-index.service';
 import { ChartPerformanceDataRetrieval } from '../../chart.performance.data.retrieval';
 import { IChartProps } from '../../charts.models/IChartProps';
 
@@ -9,9 +8,9 @@ import { IChartProps } from '../../charts.models/IChartProps';
   templateUrl: './chart-hospitality.component.html',
   styleUrls: ['./chart-hospitality.component.css']
 })
-export class ChartHospitalityComponent implements OnInit {
+export class ChartHospitalityComponent {
   public hospitalityMainChart: IChartProps = {};
-  loading: boolean = false;
+  @Input() loading: boolean;
   public trafficRadioGroup = new UntypedFormGroup({
     trafficRadio: new UntypedFormControl('Month')
   });
@@ -19,24 +18,14 @@ export class ChartHospitalityComponent implements OnInit {
     this.trafficRadioGroup.setValue({ trafficRadio: value });
     this.initCharts();
   }
-  constructor(private chartPerformanceDataRetrieval: ChartPerformanceDataRetrieval,
-    private performanceIndexService: PerformanceIndexService) { }
+  constructor(private chartPerformanceDataRetrieval: ChartPerformanceDataRetrieval) { }
 
-  ngOnInit(): void {
-    this.performanceIndexService.getChartData(1, 1685566800000, 1688158799000, 'Month')
-      .subscribe((result) => {
-        this.loading = true;
-        this.initData(result.hospitalityChartData[0], result.hospitalityChartData[1], result.hospitalityChartData[2])
-        this.initCharts();
-      })
-  }
 
   public initData(hapyyIndex: number[], nps: number[], average: number[]) {
     this.chartPerformanceDataRetrieval.initData(hapyyIndex, nps, average)
   }
 
   public initCharts(period: string = 'Month') {
-    console.log('initCharts')
     this.chartPerformanceDataRetrieval.initMainChart(period);
     this.hospitalityMainChart = this.chartPerformanceDataRetrieval.mainChart;
   }
