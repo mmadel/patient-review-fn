6c873220bc03dc5424b97c49d2c0c0993fd0e29c
mmadel <mohammed.adel82@gmail.com>
2023-06-20 20:49:13 +0300

    add login template and its function

--- src/app/modules/security/components/login.component.ts
+++ src/app/modules/security/components/login.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpStatusCode } from '@angular/common/http';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { AuthenticationService } from '../service/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form = {
+    userName: null,
+    password: null
+  };
+  errorMessage: string | null;
+  @ViewChild('loginForm') loginForm: NgForm;
+  constructor(private authService: AuthenticationService,
+    private router: Router, private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      this.spinner.show();
+      this.authService.login(this.form).subscribe(response => {
+        if (response.userRole === 'ADMIN')
+          this.router.navigateByUrl('/admin');
+        if (response.userRole === 'USER')
+          console.log('response.userRole === USER ')
+        this.spinner.hide();
+
+      }, err => {
+        this.spinner.hide();
+        if (err.status === HttpStatusCode.Unauthorized)
+          this.errorMessage = 'invalid username or password'
+
+      });
+    } else {
+      this.errorMessage = 'Please enter valid data';
+    }
+  }
+  resetError(): void {
+    this.errorMessage = null;
   }
 
 }
