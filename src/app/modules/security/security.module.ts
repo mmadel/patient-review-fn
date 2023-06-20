d95a4272c776ff21852e93315df76c92f7b9d149
mmadel <mohammed.adel82@gmail.com>
2023-06-20 20:04:40 +0300

    create security module

--- src/app/modules/security/security.module.ts
+++ src/app/modules/security/security.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  ButtonModule,
+  CardModule,
+  GridModule,
+  SharedModule,
+  FormModule,
 
+
+} from '@coreui/angular-pro';
+import { FormsModule } from '@angular/forms';
+import { IconModule } from '@coreui/icons-angular';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ButtonModule,
+    CardModule,
+    GridModule,
+    SharedModule,
+    FormModule,
+    IconModule,
+    NgxSpinnerModule.forRoot({ type: 'ball-scale-multiple' })
   ]
 })
 export class SecurityModule { }
