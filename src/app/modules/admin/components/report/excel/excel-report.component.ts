de43fd60c75d67c6104b1cf05237f7dc1465ddc1
mmadel <mohammed.adel82@gmail.com>
2023-06-25 21:37:49 +0300

    create skeleton excel report component

---
+++ src/app/modules/admin/components/report/excel/excel-report.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-excel-report',
+  templateUrl: './excel-report.component.html',
+  styleUrls: ['./excel-report.component.css']
+})
+export class ExcelReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
