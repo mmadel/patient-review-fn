a42cf789c4f9dcb2bac92d6b322123d88cea2523
mmadel <mohammed.adel82@gmail.com>
2023-06-26 17:17:59 +0300

    create update user component

--- src/app/modules/admin/patient-admin.module.ts
+++ src/app/modules/admin/patient-admin.module.ts
@@ -44,7 +44,8 @@ import {
   ClinicListComponent,
   ClinicCreateComponent,
   UserListComponent,
-  UserCreateComponent
+  UserCreateComponent,
+  UserUpdateComponent
 } from './components';
 
 
@@ -57,11 +58,12 @@ const APP_ADMIN_COMPONENT = [
   ClinicListComponent,
   ClinicCreateComponent,
   UserListComponent,
-  UserCreateComponent
+  UserCreateComponent,
+  UserUpdateComponent
 ]
 @NgModule({
   declarations: [
-    APP_ADMIN_COMPONENT
+    APP_ADMIN_COMPONENT,
   ],
   imports: [
     CommonModule,
