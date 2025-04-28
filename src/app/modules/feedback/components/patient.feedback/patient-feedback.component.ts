b66fa0ba25296f6e5df560a09611b3070d2b35b1
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-04-28 11:55:09 +0300

    add new reaction

--- src/app/modules/feedback/components/patient.feedback/patient-feedback.component.ts
+++ src/app/modules/feedback/components/patient.feedback/patient-feedback.component.ts
@@ -1,163 +1,103 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
-import NoSleep from 'nosleep.js';
 import { PatientFeedback } from '../../models/patient.feedback';
 import { FeedbackService } from '../../services/feedback.service';
-
 @Component({
   selector: 'app-patient-feedback',
   templateUrl: './patient-feedback.component.html',
-  styleUrls: ['./patient-feedback.component.css']
+  styleUrls: ['./patient-feedback.component.css'],
+  animations: [
+    // Fade in animation
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('300ms ease-out', style({ opacity: 1 }))
+      ])
+    ]),
+    // Slide in animation
+    trigger('slideIn', [
+      transition(':enter', [
+        style({ transform: 'translateX(100%)' }),
+        animate('400ms ease-out', style({ transform: 'translateX(0)' }))
+      ]),
+      transition(':leave', [
+        animate('400ms ease-in', style({ transform: 'translateX(-100%)' }))
+      ])
+    ])
+  ]
 })
 export class PatientFeedbackComponent implements OnInit {
   @ViewChild('search') search: ElementRef;
-  isSubmitted: boolean = false;
+  isClicked: boolean = false;
+  isSubmitted: boolean = false;;
   isClinicIdEmpty: boolean = false;
   clinicId: number;
   model: PatientFeedback;
   hospitalityToggle: any
   clinicalToggle: any
+  feedbackForm: FormGroup
+  isValidForm: boolean = false;
+  constructor(private feedbackService: FeedbackService) { }
+  currentPage: 'rating' | 'form' | 'thanks' = 'rating'; // Add 'thanks' page state
+  selectedHospitalityEmoji: any = null;
+  selectedClinicalEmoji: any = null;
+  hospitalityEmojis = [
+    { label: 'Good', image: 'assets/emojis/good.png' },
+    { label: 'Bad', image: 'assets/emojis/bad.png' },
+    { label: 'Terrible', image: 'assets/emojis/terrible.png' }
+  ];
 
-  constructor(private feedbackService: FeedbackService,
-    private spinner: NgxSpinnerService,
-    private router: Router,
-    private route: ActivatedRoute) { }
+  clinicalEmojis = [
+    { label: 'Good', image: 'assets/emojis/good.png' },
+    { label: 'Bad', image: 'assets/emojis/bad.png' },
+    { label: 'Terrible', image: 'assets/emojis/terrible.png' }
+  ];
 
   ngOnInit(): void {
-    
-    window.addEventListener("keyup", disableF5);
-    window.addEventListener("keydown", disableF5);
-
-    function disableF5(e: any) {
-      if ((e.which || e.keyCode) == 116) e.preventDefault();
-    };
-    this.isSubmitted = false;
-    this.clinicId = Number(this.route.snapshot.queryParamMap.get('clinicId'));
-    this.initModel()
-    if (this.clinicId === 0 || this.clinicId === undefined || this.clinicId === null) {
-      this.isClinicIdEmpty = true;
-    } else {
-      this.router.navigate([], {
-        queryParams: {
-          'clinicId': null,
-        },
-        queryParamsHandling: 'merge'
-      })
-    }
-    document.getElementById("no_sleep_input")?.click();
-  }
-  checkFields(): boolean {
-    var hospitalityValue: string = '';
-    var clinicalValue: string = '';
-    Object.keys(this.hospitalityToggle).forEach((key, index) => {
-      if (this.hospitalityToggle[key]) {
-        hospitalityValue = this.hospitalityToggle[key];
-        return;
-      }
-    });
-    Object.keys(this.clinicalToggle).forEach((key, index) => {
-      if (this.clinicalToggle[key]) {
-        clinicalValue = this.clinicalToggle[key];
-        return;
-      }
-    });  
-    var result: boolean = this.model.patientName == "" || (!hospitalityValue || !clinicalValue);
-    return result;
-  }
-  submit() {
-    this.populateModel();
-    this.spinner.show();
-    this.feedbackService.submit(this.model).subscribe((response) => {
-      this.isSubmitted = true;
-      this.spinner.hide();
-      setTimeout(() => {
-        //window.location.reload()
-        this.router.navigateByUrl('feedback/submit', { skipLocationChange: true }).then(() => {
-          this.router.navigate(['feedback/submit'], { queryParams: { clinicId: this.clinicId } }).then(() => {
-            this.ngOnInit()
-          })
-        })
-      }, 5000);  //5s
+    this.feedbackForm = new FormGroup({
+      'firstName': new FormControl(null, [Validators.required]),
+      'lastName': new FormControl(null, [Validators.required]),
+      'comments': new FormControl(null),
     })
   }
-  public selectFeedback(event: any) {
-    var controlName: string[] = event.target.name.split("-", 2);
-    var feedbackFeeling = controlName[1];
-    var entityContainer: any = controlName[0] === 'hospitality' ? this.hospitalityToggle : this.clinicalToggle;
-    switch (feedbackFeeling) {
-      case "VeryGood":
-        entityContainer.VGood = true;
-        entityContainer.Good = false, entityContainer.VSad = false, entityContainer.Sad = false
-        break;
-      case "Happy":
-        entityContainer.Good = true;
-        entityContainer.VGood = false, entityContainer.VSad = false, entityContainer.Sad = false
-        break;
-      case "Meh":
-        entityContainer.Sad = true;
-        entityContainer.VGood = false, entityContainer.Good = false, entityContainer.VSad = false
-        break;
-      case "Frown":
-        entityContainer.VSad = true;
-        entityContainer.VGood = false, entityContainer.Good = false, entityContainer.Sad = false
-        break;
+  bothSelected(): boolean {
+    return !!this.selectedHospitalityEmoji && !!this.selectedClinicalEmoji;
+  }
+  selectEmoji(serviceType: string, emoji: any) {
+    if (serviceType === 'hospitality') {
+      this.selectedHospitalityEmoji = emoji;
+    } else {
+      this.selectedClinicalEmoji = emoji;
+    }
+
+    if (this.bothSelected()) {
+      setTimeout(() => {
+        this.currentPage = 'form';
+      }, 800);
+    }
+  }
+  goBack() {
+    this.currentPage = 'rating';
+    // Reset the selected emojis
+    if (this.isSubmitted) {
+      this.selectedHospitalityEmoji = null;
+      this.selectedClinicalEmoji = null;
+      this.isSubmitted = false;
     }
   }
 
-  private populateModel() {
-    let hospitalityValue: string = '';
-    let clinicalValue: string = '';
-    this.model.clinicId = this.clinicId;
-    Object.keys(this.hospitalityToggle).forEach((key, index) => {
-      if (this.hospitalityToggle[key]) {
-        hospitalityValue = key;
-        return;
-      }
-    });
-    Object.keys(this.clinicalToggle).forEach((key, index) => {
-      if (this.clinicalToggle[key]) {
-        clinicalValue = key;
-        return;
-      }
-    });
-    this.model.feedbackQuestions = {
-      hospitalityFeedback: hospitalityValue,
-      clinicalFeedback: clinicalValue
-
+  submitFeedback() {
+    this.isClicked = true
+    if (this.feedbackForm?.valid) {
+      setTimeout(() => {
+        this.currentPage = 'thanks'; // Switch to the thanks page after submitting
+        this.isSubmitted = true;
+      }, 800); // Simulate a small delay before transition
+    } else {
+      this.isValidForm = true;
     }
-  }
-  private initModel() {
-    var model: PatientFeedback = {
-      clinicId: 0,
-      feedbackQuestions: {
-        hospitalityFeedback: '',
-        clinicalFeedback: ''
-      },
-      patientName: '',
-    }
-    this.model = model;
-    var hospitalityToggle: any = {
-      VGood: false,
-      Good: false,
-      VSad: false,
-      Sad: false
-    }
-    this.hospitalityToggle = hospitalityToggle
-    var clinicalToggle: any = {
-      VGood: false,
-      Good: false,
-      VSad: false,
-      Sad: false
-    }
-    this.clinicalToggle = clinicalToggle;
-  }
-  handleNoSleep(){
-    console.log('clickeed')
-    var noSleep = new NoSleep();
-    document.addEventListener('click', function enableNoSleep() {
-      document.removeEventListener('click', enableNoSleep, false);
-      noSleep.enable();
-    }, false);
   }
 }
