514c8c4ad30582b734c6f754c3c50e7a29d22ca3
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-05-08 16:10:07 +0300

    remove fixed ip in baseURL

--- src/app/modules/admin/components/feedback.create/feedback-create.component.ts
+++ src/app/modules/admin/components/feedback.create/feedback-create.component.ts
@@ -11,7 +11,7 @@ export class FeedbackCreateComponent implements OnInit {
   public createFeedbackURL: string
   public clinicId: number | null;
   public clinicName: string | null;
-  public baseURL: string = "http://192.168.2.180:4200";
+  public baseURL: string = location.origin;
   constructor(private clinicService: ClinicService) { }
 
   ngOnInit(): void {
